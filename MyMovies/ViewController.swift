4a4403fd8c616b7bde6cac2f5a9f7dd952978d6d
Helton Isac <helton.isac@gmail.com>
2020-04-14 21:51:45 -0300

    adding table to the project

--- MyMovies/ViewController.swift
+++ MyMovies/ViewController.swift
@@ -10,30 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var movies: [Movie] = []
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadMovies()
-    }
-    
-    func loadMovies() {
-        guard let fileURL = Bundle.main.url(forResource: "movies", withExtension: "json" )
-            else { return }
-        do {
-            let data = try Data(contentsOf: fileURL)
-            
-            movies = try JSONDecoder().decode([Movie].self,
-                                              from: data)
-            
-            for movie in movies {
-                print(movie.title, "-", movie.duration)
-            }
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-    
+    }    
     
 }
 
