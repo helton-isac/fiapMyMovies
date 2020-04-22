f9128a38c315a5b05fc834fc6381b86a64405f09
Helton Isac <helton.isac@gmail.com>
2020-04-21 22:47:00 -0300

    Using Alert, Notification and WebServices

---
+++ MyMovies/API.swift
@@ -0,0 +1,73 @@
+//
+//  API.swift
+//  MyMovies
+//
+//  Created by Helton Isac Torres Galindo on 21/04/20.
+//  Copyright © 2020 Helton Isac Torres Galindo. All rights reserved.
+//
+
+import Foundation
+
+struct APIResult: Codable {
+    let results: [Trailer]
+}
+
+struct Trailer: Codable {
+    let previewUrl: String
+}
+
+class API {
+    
+    static let basePath = "https://itunes.apple.com/search?media=movie&amp;entity=movie&amp;term="
+    
+    static let configuration: URLSessionConfiguration = {
+        
+        let config = URLSessionConfiguration.default
+        config.allowsCellularAccess = true
+        config.httpAdditionalHeaders = ["Content-Type": "application/json"]
+        config.timeoutIntervalForRequest = 45.0
+        config.httpMaximumConnectionsPerHost = 5
+        return config
+    }()
+    
+    static let session = URLSession(configuration: configuration)
+    
+    static func loadTrailers(title: String, onComplete: @escaping (APIResult?) -> Void) {
+        
+        guard let encodedTitle = (title).addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
+            
+            let url = URL(string: basePath+encodedTitle) else {
+                onComplete(nil)
+                return
+        }
+        
+        let task = session.dataTask(with: url) {
+            (data: Data?, response: URLResponse?, error: Error?) in
+            
+            if error != nil {
+                print(error!.localizedDescription)
+                onComplete(nil)
+            } else {
+                guard let response = response as? HTTPURLResponse else {
+                    onComplete(nil)
+                    return
+                }
+                if response.statusCode == 200 {
+                    guard let data = data else {
+                        onComplete(nil)
+                        return
+                    }
+                    do {
+                        let apiResult = try JSONDecoder().decode(APIResult.self, from: data)
+                        onComplete(apiResult)
+                    } catch {
+                        onComplete(nil)
+                    }
+                } else {
+                    onComplete(nil)
+                }
+            }
+        }
+        task.resume()
+    }
+}
--- MyMovies/ViewController.swift
+++ MyMovies/ViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
     
     var movie: Movie?
+    
+    var trailer = ""
     
     @IBOutlet weak var ivMovie: UIImageView!
     @IBOutlet weak var lbTitle: UILabel!
@@ -19,8 +22,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var lbRating: UILabel!
     @IBOutlet weak var tvSummary: UITextView!
     
+    var datePicker = UIDatePicker()
+    
+    var alert: UIAlertController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let title = movie?.title {
+            loadTrailers(title: title)
+        }
+        
+        datePicker.minimumDate = Date()
+        
+        datePicker.addTarget(self, action: #selector(changeDate), for: .valueChanged)
+    }
+    
+    @objc func changeDate() {
+        
+        let dateformatter = DateFormatter()
+        
+        dateformatter.dateFormat = "dd/MM/yyyy HH:mm'h"
+        
+        alert.textFields?.first?.text = dateformatter.string(from: datePicker.date)
+    }
+    
+    func loadTrailers(title: String) {
+        
+        API.loadTrailers(title: title) { (apiResult) in
+            guard
+                let results = apiResult?.results,
+                let trailer = results.first else {
+                    return
+                    
+            }
+            
+            DispatchQueue.main.async {
+                self.trailer = trailer.previewUrl
+                print("URL do Trailer:", trailer.previewUrl)
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) { super.viewWillAppear(animated)
@@ -35,10 +76,46 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func scheduleNotification(_ sender: UIButton) {
+        
+        alert = UIAlertController(title: "Lembrete", message: "Quando deseja ser lembrado de assistir o filme?", preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Data do lembrete"
+            self.datePicker.date = Date()
+            textField.inputView = self.datePicker
+        }
+        
+        let okAction = UIAlertAction(title: "Agendar", style: .default) { (action) in
+            self.schedule()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+
+    func schedule() {
+        let content = UNMutableNotificationContent()
+        let movieTitle = movie?.title ?? ""
+        
+        content.title = "Lembrete"
+        content.body = "Assistir filme \(movieTitle)"
+        
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: datePicker.date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        let request = UNNotificationRequest(identifier: "Lembrete", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let vc = segue.destination as? AddEditViewController {
             vc.movie = movie
         }
     }
 }
-
