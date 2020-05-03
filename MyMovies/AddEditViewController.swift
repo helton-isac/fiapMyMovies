60b10e32a34d1b55c5e45cbd12606a21e16dc879
Helton Isac <helton.isac@gmail.com>
2020-05-03 18:36:58 -0300

    Adicionando possibilidade de Inserir Imagem

--- MyMovies/AddEditViewController.swift
+++ MyMovies/AddEditViewController.swift
@@ -59,4 +59,65 @@ class AddEditViewController: UIViewController {
             print(error.localizedDescription)
         }
     }
+    
+    @IBAction func addPoster(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Selecionar pôster",
+                                      message: "De onde você quer escolher o pôster?",
+                                      preferredStyle: .actionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Câmera", style: .default) {
+                (action) in self.selectPicture(source: .camera)
+            }
+            alert.addAction(cameraAction)
+        }
+        
+        let libraryAction = UIAlertAction(title: "Biblioteca de Fotos", style: .default) {
+            (action) in self.selectPicture(source: .photoLibrary)
+        }
+        alert.addAction(libraryAction)
+        
+        let photosAction = UIAlertAction(title: "Álbum de Fotos", style: .default) {
+            (action) in self.selectPicture(source: .photoLibrary)
+        }
+        alert.addAction(photosAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func selectPicture(source: UIImagePickerController.SourceType) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = source
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+
+extension AddEditViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image =
+            info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            let aspectRatio = image.size.width / image.size.height
+            var smallSize: CGSize
+            if aspectRatio > 1 {
+                smallSize = CGSize(width: 800, height: 800 / aspectRatio)
+            } else {
+                smallSize = CGSize(width: 800 * aspectRatio, height: 800)
+            }
+            
+            UIGraphicsBeginImageContext(smallSize)
+            image.draw(in: CGRect(x: 0, y: 0, width: smallSize.width, height: smallSize.height))
+            let smallImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            ivMovie.image = smallImage
+            
+            dismiss(animated: true, completion: nil)
+        }
+    }
+        
 }
